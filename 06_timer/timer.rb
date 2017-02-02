0fdbb46c55edadd4e92aecc643978b0c89423857
Niklas Kaiser <kaiser_niklas@live.de>
2017-02-02 11:31:41 +0100

    dd

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+
+	attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+	end
+
+	def time_string
+		hours = @seconds / 3600
+		minutes = @seconds % 3600
+		en_min = minutes / 60
+		seconds = minutes % 60
+
+		sprintf('%02d:%02d:%02d', hours.to_s, en_min.to_s, seconds.to_s)
+
+	end
+
+	
+
+
+
+  
 end
